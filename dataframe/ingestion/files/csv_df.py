ba3817a586078fa2cc3d3cfa3b727cc92808c84c
muralipatnam <murali.patnam@gmail.com>
2021-07-13 23:50:50 -0400

    trying hive

--- dataframe/ingestion/files/csv_df.py
+++ dataframe/ingestion/files/csv_df.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
     spark.sql(set_dynamic_mode)
     create_partitioned_sql = "CREATE TABLE IF NOT EXISTS abc_part (income INT) PARTITIONED BY (id INT)"
     spark.sql(create_partitioned_sql)
-    insert_sql = "INSERT INTO abc_part PARTITION (id) SELECT income FROM mytable"
+    insert_sql = "INSERT INTO abc_part PARTITION (id) SELECT id,income FROM mytable"
     spark.sql(insert_sql)
     spark.sql("show partitions abc_part")
     spark.stop()
