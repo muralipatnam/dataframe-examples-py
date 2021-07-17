c03088d44ca68d7577518d8e5b712f1536d2263c
muralipatnam <murali.patnam@gmail.com>
2021-07-17 01:00:23 -0400

    added sql more functions

---
+++ dataframe/curation/sql/more_functions.py
@@ -0,0 +1,22 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import first,trim,lower,ltrim,initcap,format_string,coalesce,lit,col
+from model.Person import Person
+
+if __name__ == '__main__':
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("DSL examples") \
+        .master('local[*]') \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    people_df = spark.createDataFrame([
+        Person("Sidhartha", "Ray", 32, None, "Programmer"),
+        Person("Pratik", "Solanki", 22, 176.7, None),
+        Person("Ashok ", "Pradhan", 62, None, None),
+        Person(" ashok", "Pradhan", 42, 125.3, "Chemical Engineer"),
+        Person("Pratik", "Solanki", 22, 222.2, "Teacher")
+    ])
+
+    people_df.show()
