9ca36319612f092c4e9a8410e495b8218937fba9
muralipatnam <murali.patnam@gmail.com>
2021-07-17 03:30:34 -0400

    added sql more functions

---
+++ dataframe/curation/sql/HiveExamples.py
@@ -0,0 +1,30 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, IntegerType, BooleanType,DoubleType
+import os.path
+import yaml
+
+if __name__ == '__main__':
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read Files") \
+        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
+        .enableHiveSupport()\
+        .getOrCreate()
+        # .master('local[*]') \
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    student_df = spark.sql("SELECT * FROM students")
+    student_df.show()
+    spark.stop()
+
+    # spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" dataframe/curation/sql/HiveExamples.py
